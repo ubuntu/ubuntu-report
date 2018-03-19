e1d0fe9b0fd42a471d2d9a5d111945f96834708e
Didier Roche <didrocks@ubuntu.com>
2018-03-19 13:46:37 +0100

    Running Collect twice should return the same json fields and values

--- internal/metrics/metrics_test.go
+++ internal/metrics/metrics_test.go
@@ -2,6 +2,7 @@ package metrics_test
 
 import (
 	"context"
+	"encoding/json"
 	"os/exec"
 	"path"
 	"testing"
@@ -91,6 +92,59 @@ func TestCollect(t *testing.T) {
 			want := helper.LoadOrUpdateGolden(path.Join(tc.root, "gold", "collect"), got, *metrics.Update, t)
 			a.CheckWantedErr(err, tc.wantErr)
 			a.Equal(got, want)
+		})
+	}
+}
+
+func TestRunCollectTwice(t *testing.T) {
+	testCases := []struct {
+		name          string
+		root          string
+		caseGPU       string
+		caseScreen    string
+		casePartition string
+		env           map[string]string
+
+		// note that only an internal json package error can make it returning an error
+		wantErr bool
+	}{
+		{"regular",
+			"testdata/good", "one gpu", "one screen", "one partition",
+			map[string]string{"XDG_CURRENT_DESKTOP": "some:thing", "XDG_SESSION_DESKTOP": "ubuntusession", "XDG_SESSION_TYPE": "x12"},
+			false},
+		{"empty",
+			"testdata/none", "empty", "empty", "empty",
+			nil,
+			false},
+	}
+	for _, tc := range testCases {
+		tc := tc // capture range variable for parallel execution
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			a := helper.Asserter{T: t}
+
+			cmdGPU, cancel := newMockShortCmd(t, "lspci", "-n", tc.caseGPU)
+			defer cancel()
+			cmdScreen, cancel := newMockShortCmd(t, "xrandr", tc.caseScreen)
+			defer cancel()
+			cmdPartition, cancel := newMockShortCmd(t, "df", tc.casePartition)
+			defer cancel()
+
+			m := newTestMetrics(t, metrics.WithRootAt(tc.root),
+				metrics.WithGPUInfoCommand(cmdGPU),
+				metrics.WithScreenInfoCommand(cmdScreen),
+				metrics.WithSpaceInfoCommand(cmdPartition),
+				metrics.WithMapForEnv(tc.env))
+			b1, err1 := m.Collect()
+			b2, err2 := m.Collect()
+
+			a.CheckWantedErr(err1, tc.wantErr)
+			a.CheckWantedErr(err2, tc.wantErr)
+			var got1, got2 json.RawMessage
+			json.Unmarshal(b1, got1)
+			json.Unmarshal(b2, got2)
+
+			a.Equal(got1, got2)
 		})
 	}
 }
