11cd6d0ee848ea5fd30515f246588665aef25251
Didier Roche <didrocks@ubuntu.com>
2018-03-21 12:24:27 +0100

    generate commands in README.md from cobra output
    That enables us to stay in sync with the command syntax by simply running
    go test --generate in the cmd/ubuntu-report package.
    We rewrite on demand the README.md file, skipping the previous usage section
    and do this on all subcommands, only extracting the relevant part (skipping
    SEE ALSO and such). Finally, we skip the interactive details as it's an alias
    to running "ubuntu-report".

--- cmd/ubuntu-report/generate_test.go
+++ cmd/ubuntu-report/generate_test.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"flag"
+	"io"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
 	"github.com/spf13/cobra/doc"
 )
 
@@ -51,5 +56,98 @@ func TestGenerateCompletion(t *testing.T) {
 	}
 	if err := rootCmd.GenZshCompletionFile(filepath.Join(out, "zsh-completion")); err != nil {
 		t.Fatalf("couldn't generate bazshsh completion: %v", err)
+	}
+}
+
+func TestGenerateREADME(t *testing.T) {
+	if !*generate {
+		t.Skip("skipping README generation, --generate isn't set")
+	}
+	t.Parallel()
+
+	sp := filepath.Join("..", "..", "README.md")
+	dp := sp + ".new"
+	src, err := os.Open(sp)
+	if err != nil {
+		t.Fatalf("couldn't open %s: %v", sp, err)
+	}
+	defer src.Close()
+	dst, err := os.Create(dp)
+	if err != nil {
+		t.Fatalf("couldn't create %s: %v", dp, err)
+	}
+	defer dst.Close()
+
+	// write start of README
+	scanner := bufio.NewScanner(src)
+	for scanner.Scan() {
+		txt := scanner.Text()
+		mustWrite(t, dst, txt)
+		if txt == "## Command line usage" {
+			mustWrite(t, dst, "")
+			break
+		}
+	}
+
+	// write generated command line
+	cmds := []*cobra.Command{generateRootCmd()}
+	cmds = append(cmds, cmds[0].Commands()...)
+	for _, cmd := range cmds {
+		pr, pw := io.Pipe()
+
+		go func() {
+			if err := doc.GenMarkdown(cmd, pw); err != nil {
+				pw.CloseWithError(errors.Wrapf(err, "generate markdown for %s failed", cmd.Name()))
+				return
+			}
+			pw.Close()
+		}()
+
+		mScanner := bufio.NewScanner(pr)
+		for mScanner.Scan() {
+			txt := mScanner.Text()
+			if strings.HasPrefix(txt, "### SEE ALSO") {
+				break
+			}
+			// interactive is an alias to ubuntu-report only, don't file up more info
+			if cmd.Name() == "interactive" && strings.HasPrefix(txt, "### Synopsis") {
+				break
+			}
+			// add a subindentation
+			if strings.HasPrefix(txt, "##") {
+				txt = "#" + txt
+			}
+			mustWrite(t, dst, txt)
+		}
+
+		if err = mScanner.Err(); err != nil {
+			t.Fatalf("error while reading generated markdown for %s: %v", cmd.Name(), err)
+		}
+	}
+
+	// skip to next paragraph (ignore previous generation) and write to the end of file
+	skip := true
+	for scanner.Scan() {
+		txt := scanner.Text()
+		if skip && strings.HasPrefix(txt, "## ") {
+			skip = false
+		}
+		if !skip {
+			mustWrite(t, dst, txt)
+		}
+	}
+	if err = scanner.Err(); err != nil {
+		t.Fatalf("error while reading %s: %v", sp, err)
+	}
+
+	dst.Close()
+	if err = os.Rename(dp, sp); err != nil {
+		t.Fatalf("couldn't rename %s to %s: %v", dp, sp, err)
+	}
+}
+
+func mustWrite(t *testing.T, f *os.File, s string) {
+	if _, err := f.WriteString(s + "\n"); err != nil {
+		t.Fatalf("couldn't write '%s' to %s: %v", s, f.Name(), err)
 	}
 }
