6292bacea062daf82a18dda9815b842111ddf8ea
Didier Roche <didrocks@ubuntu.com>
2018-03-23 13:40:10 +0100

    C API for sysmetrics with documentation and example
    Exposed sysmetrics go API to a C API with examples on how to use and build it.

---
+++ pkg/sysmetrics/C/doc.go
@@ -0,0 +1,73 @@
+// C bindings to sysmetrics package
+//
+// Building as a shared library
+//
+// The following command (in the pkg/sysmetrics/C directory) will provide a .h and .so file:
+//   go build -o libsysmetrics1.so -buildmode=c-shared libsysmetrics.go
+//
+// Then, you can simply build your example program with:
+//   gcc main.c ./libsysmetrics1.so
+//
+// Collect system info
+//
+// res will be the pretty printed version of collected data.
+// err will be != NULL in case any errored occured.
+//
+//   char* Collect(char** res);
+//
+// Example:
+//   #include <stdio.h>
+//   #include <stdlib.h>
+//   #include "libsysmetrics1.h"
+//
+//   int main() {
+//       char *res, *err;
+//
+//       err = Collect(&res);
+//
+//       if (err != NULL) {
+//           printf("ERR: %s\n", err);
+//       } else {
+//           printf("GOT: %s\n", res);
+//       }
+//       free(res);
+//       free(err);
+//   }
+//
+// CollectAndSend gather system info and send them
+//
+// alwaysReports bypass previous report already be sent for current version check.
+// It can be send to an alternative url via baseURL to send the report to, if not empty.
+//
+//   char* CollectAndSend(ReportType r, bool alwaysreport, char* url);
+//
+// ReportType is the following enum:
+//    typedef enum {
+//      // ReportInteractive will show report content on stdout and read anwser on stdin
+//      ReportInteractive = 0,
+//      // ReportAuto will send a report without printing report
+//      ReportAuto = 1,
+//      // ReportOptOut will send opt-out message without printing report
+//      ReportOptOut = 2,
+//    } ReportType;
+//
+// Example (in autoreport mode, without reporting twice the same data and using default server URL):
+//   #include <stdbool.h>
+//   #include <stdio.h>
+//   #include <stdlib.h>
+//   #include "libsysmetrics1.h"
+//
+//   int main() {
+//       ReportType r = ReportAuto;
+//       char *err;
+//
+//       err = CollectAndSend(r, false, "");
+//
+//       if (err != NULL) {
+//           printf("ERR: %s\n", err);
+//       } else {
+//           printf("Report sent to default server");
+//       }
+//       free(err);
+//   }
+package main
---
+++ pkg/sysmetrics/C/libsysmetrics.go
@@ -0,0 +1,46 @@
+package main
+
+/*
+// ReportType define the desired kind of interaction in CollectAndSend()
+typedef enum {
+    // ReportInteractive will show report content on stdout and read anwser on stdin
+    ReportInteractive = 0,
+    // ReportAuto will send a report without printing report
+    ReportAuto = 1,
+    // ReportOptOut will send opt-out message without printing report
+    ReportOptOut = 2,
+} ReportType;
+*/
+import "C"
+
+import (
+	"github.com/ubuntu/ubuntu-report/pkg/sysmetrics"
+)
+
+// Collect system info and return a pretty printed version of collected data
+//export Collect
+func Collect(res **C.char) *C.char {
+	b, err := sysmetrics.Collect()
+	*res = C.CString(string(b))
+	if err != nil {
+		*res = C.CString("") // scratch data
+		return C.CString(err.Error())
+	}
+	return nil
+}
+
+// CollectAndSend gather system info and send them
+// alwaysReports bypass previous report already be sent for current version check
+// It can be send to an alternative url via baseURL to send the report to, if not empty
+//export CollectAndSend
+func CollectAndSend(r C.ReportType, alwaysReport bool, baseURL *C.char) *C.char {
+	err := sysmetrics.CollectAndSend(sysmetrics.ReportType(r), alwaysReport, C.GoString(baseURL))
+	if err != nil {
+		return C.CString(err.Error())
+	}
+	return nil
+}
+
+func main() {
+
+}
