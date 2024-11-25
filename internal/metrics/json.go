f5b4b1053d0d4fa65f70c2a35b952820d477391d
kat <28567881+hk21702@users.noreply.github.com>
2024-11-25 13:24:41 -0500

    fix: lscpu json parsing
    Fixes CPU reporting by implementing proper JSON parsing and implementing it for lscpu -J.
    Tests have been updated to now better reflect lscpu -J's output (Whitespace, new lines, and children)
    ---
    UDENG-5408

---
+++ internal/metrics/json.go
@@ -0,0 +1,32 @@
+package metrics
+
+import (
+	"encoding/json"
+	"io"
+
+	"github.com/pkg/errors"
+)
+
+type LscpuEntry struct {
+	Field    string       `json:"field"`
+	Data     string       `json:"data"`
+	Children []LscpuEntry `json:"children,omitempty"`
+}
+
+type Lscpu struct {
+	Lscpu []LscpuEntry `json:"lscpu"`
+}
+
+func parseJSON(r io.Reader, structPtr interface{}) (interface{}, error) {
+	// Read the entire content of the io.Reader first to check for errors even if valid json is first
+	buf, err := io.ReadAll(r)
+	if err != nil {
+		return nil, errors.Errorf("error reading from io.Reader: %v", err)
+	}
+
+	err = json.Unmarshal(buf, structPtr)
+	if err != nil {
+		return nil, errors.Errorf("couldn't parse JSON: %v", err)
+	}
+	return structPtr, nil
+}
